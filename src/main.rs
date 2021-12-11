ccbc4d2c6a5ebe8c8f17ce1f6c0d0491ae268934
beyarkay <boydrkane@gmail.com>
2021-12-12 00:47:17 +0200

    Output card formatting changes

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,15 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let filename: String;
     if args.len() != 2 {
-        println!("Using `games/chameleon.yaml` as default gamefile.");
-        filename = "games/chameleon.yaml".to_string();
+        println!("Using `games/red_flags.yaml` as default gamefile.");
+        filename = "games/red_flags.yaml".to_string();
     } else {
         filename = format!("{}", args[1]);
     }
     let f = std::fs::File::open(filename).expect("Could not open file.");
-    let chameleon_game: Game = serde_yaml::from_reader(f).expect("Could not read values.");
+    let game: Game = serde_yaml::from_reader(f).expect("Could not read values.");
     // Create one PDF per expansion
-    for expansion in chameleon_game.expansions {
+    for expansion in game.expansions {
         println!("Writing expansion '{}'", expansion.name);
         let mut pdf = format!("\\documentclass[config, grid, fronts]{{flashcards}}\n\
                             \\cardfrontstyle[\\LARGE]{{headings}}\n\
@@ -25,24 +25,16 @@ fn main() {
                             \t\\cardfrontfoot{{{0}}}\n\n", expansion.name);
         for category in expansion.categories {
             for item in category.items {
-                pdf.push_str(build_card_snippet(
-                        chameleon_game.name.as_str(),
-                        item.as_str()
-                        ).as_str());
+                pdf.push_str(format!(
+                        "\t\\begin{{flashcard}}[{0} ({1})] {{{2}}} \\end{{flashcard}}\n",
+                        game.name, category.name, item).as_str());
             }
         }
         pdf.push_str("\n\\end{document}\n");
-        let filename = format!("tex/{}: {}.tex", chameleon_game.name, expansion.name);
+        let filename = format!("tex/{}: {}.tex", game.name, expansion.name);
         let mut output = File::create(filename).expect("File could not be created");
         write!(output, "{}", pdf).expect("File could not be written to");
     }
-
-}
-
-fn build_card_snippet<'a>(game_name: &'a str, content: &'a str) -> String {
-    // Curly braces are escaped by doubling them:
-    return format!("\t\\begin{{flashcard}}[{0}] {{{1}}} \\end{{flashcard}}\n",
-            game_name, content);
 }
 
 #[derive(Debug, Serialize, Deserialize)]
