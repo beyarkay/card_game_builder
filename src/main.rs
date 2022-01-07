dadd41514809152d87915519eefd487e4c397ba8
Boyd Kane <boydkane@amazon.com>
2022-01-07 20:50:37 +0200

    Autobuild yaml files to pdfs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::process::Command;
 use serde_yaml::{self};
 use std::fs::File;
 use std::io::Write;
@@ -19,22 +20,44 @@ fn main() {
     // Create one PDF per expansion
     for expansion in game.expansions {
         println!("Writing expansion '{}'", expansion.name);
-        let mut pdf = format!("\\documentclass[config, grid, fronts]{{flashcards}}\n\
-                            \\cardfrontstyle[\\LARGE]{{headings}}\n\
-                            \\begin{{document}}\n\
-                            \t\\cardfrontfoot{{{0}}}\n\n", expansion.name);
+        let mut pdf = format!("\\documentclass[playing_cards, grid, fronts]{{flashcards}}\n\
+                              \\cardbackstyle{{empty}}\n\
+                              \\cardfrontstyle[\\LARGE]{{headings}}\n\
+                              \\begin{{document}}\n\
+                              \t\\cardfrontfoot{{{0}}}\n\n", 
+                              escape_latex(expansion.name.as_str()));
         for category in expansion.categories {
             for item in category.items {
                 pdf.push_str(format!(
                         "\t\\begin{{flashcard}}[{0} ({1})] {{{2}}} \\end{{flashcard}}\n",
-                        game.name, category.name, item).as_str());
+                        escape_latex(game.name.as_str()), 
+                        escape_latex(category.name.as_str()), 
+                        escape_latex(item.as_str())).as_str());
             }
         }
         pdf.push_str("\n\\end{document}\n");
         let filename = format!("tex/{}: {}.tex", game.name, expansion.name);
-        let mut output = File::create(filename).expect("File could not be created");
+        let mut output = File::create(filename.as_str()).expect("File could not be created");
         write!(output, "{}", pdf).expect("File could not be written to");
+        // Now write the tex file to pdf
+        if cfg!(target_os = "windows") {
+            println!("Sorry, I don't know how to convert .tex files to .pdf files on windows");
+        } else {
+            let _out = Command::new("sh").arg("-c")
+                .arg(format!("pdflatex --output-dir=pdfs {}", filename.replace(" ", "\\ ")))
+                .output() // Output is smothered
+                .expect("Failed to compile pdf");
+            // also clean up the LaTeX build files
+            let _cleanup = Command::new("sh").arg("-c")
+                .arg("rm pdfs/*.out pdfs/*.aux pdfs/*.log tex/*.tex")
+                .output() // Output is smothered
+                .expect("Failed to clean up LaTeX files");
+            //println!("{}", String::from_utf8_lossy(&out.stdout));
+        }
     }
+}
+fn escape_latex(s: &str) -> String {
+    return s.replace("_", "\\_");
 }
 
 #[derive(Debug, Serialize, Deserialize)]
