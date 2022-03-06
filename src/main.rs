2d82ad6546f2aeb97983ab086049520630c4edaa
beyarkay <boydrkane@gmail.com>
2022-03-06 12:50:47 +0200

    Rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use serde::{Deserialize, Serialize};
-use std::process::Command;
 use serde_yaml::{self};
+use std::env;
 use std::fs::File;
 use std::io::Write;
-use std::env;
+use std::process::Command;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -19,52 +19,65 @@ fn main() {
     // Create one PDF per expansion
     for expansion in game.expansions {
         println!("Writing expansion '{}'...", expansion.name);
-        let mut pdf = format!("\\documentclass[playing_cards, grid, fronts]{{flashcards}}\n\
+        let mut pdf = format!(
+            "\\documentclass[playing_cards, grid, fronts]{{flashcards}}\n\
                               \\cardbackstyle{{empty}}\n\
                               \\cardfrontstyle[\\LARGE]{{headings}}\n\
                               \\begin{{document}}\n\
-                              \t\\cardfrontfoot{{{0}}}\n\n", 
-                              escape_latex(expansion.name.as_str()));
+                              \t\\cardfrontfoot{{{0}}}\n\n",
+            escape_latex(expansion.name.as_str())
+        );
         for category in expansion.categories {
             for item in category.items {
-                pdf.push_str(format!(
+                pdf.push_str(
+                    format!(
                         "\t\\begin{{flashcard}}[{0} ({1})] {{{2}}} \\end{{flashcard}}\n",
-                        escape_latex(game.name.as_str()), 
-                        escape_latex(category.name.as_str()), 
-                        escape_latex(item.as_str())).as_str());
+                        escape_latex(game.name.as_str()),
+                        escape_latex(category.name.as_str()),
+                        escape_latex(item.as_str())
+                    )
+                    .as_str(),
+                );
             }
         }
         pdf.push_str("\n\\end{document}\n");
         let filename = format!("tex/{}: {}.tex", game.name, expansion.name);
         let mut output = File::create(filename.as_str()).expect("File could not be created");
         write!(output, "{}", pdf).expect("File could not be written to");
-        println!("Expansion {} successfully written as {}, converting to pdf", expansion.name, filename);
+        println!(
+            "Expansion {} successfully written as {}, converting to pdf",
+            expansion.name, filename
+        );
         // Now write the tex file to pdf
         if cfg!(target_os = "windows") {
             println!("Sorry, I don't know how to convert .tex files to .pdf files on windows");
         } else {
             let _out = Command::new("sh")
                 .arg("-c")
-                .arg(format!("pdflatex --output-dir=pdfs {}", filename.replace(" ", "\\ ")))
+                .arg(format!(
+                    "pdflatex --output-dir=pdfs {}",
+                    filename.replace(" ", "\\ ")
+                ))
                 .output()
                 .expect("Failed to compile pdf");
             // io::stdout().write_all(&out.stdout).unwrap();
 
             // also clean up the LaTeX build files
-           let _cleanup = Command::new("sh")
-               .arg("-c")
-               .arg("rm pdfs/*.out pdfs/*.aux pdfs/*.log")
-               .output() // Output is smothered
-               .expect("Failed to clean up LaTeX files");
+            let _cleanup = Command::new("sh")
+                .arg("-c")
+                .arg("rm pdfs/*.out pdfs/*.aux pdfs/*.log")
+                .output() // Output is smothered
+                .expect("Failed to clean up LaTeX files");
         }
     }
 }
 fn escape_latex(s: &str) -> String {
-    return s.replace("_", "\\_")
-            .replace("^", "\\^")
-            .replace("&", "\\&")
-            .replace("$", "\\$")
-            .replace("%", "\\%");
+    return s
+        .replace("_", "\\_")
+        .replace("^", "\\^")
+        .replace("&", "\\&")
+        .replace("$", "\\$")
+        .replace("%", "\\%");
 }
 
 #[derive(Debug, Serialize, Deserialize)]
